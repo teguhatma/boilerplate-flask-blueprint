2194b26bd502342072162baa0cab5449b358eabb
teguhatma <teguhatmayudha@gmail.com>
2021-03-06 13:45:20 +0700

    Updating .gitignore For VSCode

--- app/admin/routes/dashboard.py
+++
@@ -1,7 +0,0 @@
-from .. import admin
-from flask import render_template
-
-
-@admin.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html", title="Dashboard")
--- setup.py
+++
@@ -1,6 +0,0 @@
-from app import create_app
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-app = create_app(os.getenv("FLASK_ENV"))
